022d9999e6cc3d4478d1c03b590e42245f3a97f0
adriancarranza <adriancarranza0113@gmail.com>
2023-12-10 21:34:55 -0800

    frontend assessment complete

--- preparedness-frontend/src/App.js
+++ preparedness-frontend/src/App.js
@@ -1,20 +1,29 @@
-import React from "react"
+import React, { useState } from 'react'
 import { Button, Input, Label } from "reactstrap"
 import ModalComponent from "./components/ModalComponent"
 import "./App.css"
 
-const App = () => {
+const App = () => {  
+  const [modal, setModal] = useState(false);
+  const toggle = () => setModal(!modal); 
+//create state using name as the value and initialized as an empty string
+  const [name, setName] = useState ("");
+// create a handle change that targets the input field, takes that value, and sets it to that state  
+  const handleChange = (inputName) => {
+    setName(inputName.target.value)
+  };
+
   return (
     <div className="entire-content">
       <h1>Preparedness Assessment</h1>
       <div className="form">
         <div className="input">
           <Label for="name">Enter your name</Label>
-          <Input />
+          <Input type="text" value={name} onChange={handleChange} /> 
         </div>
-        <Button>Click Me</Button>
-        <Button>Reset</Button>
-        <ModalComponent />
+        <Button color="danger" onClick={() => {setModal(true)}}>Click Me</Button>
+        {modal && <ModalComponent toggle={toggle} name={name}/>}
+        <Button onClick={() => {setName("")}}>Reset</Button>
       </div>
     </div>
   )
